13784b4ae72eea32a92d241756c0b12064b1e9cd
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-09-17 19:58:01 +0500

    added rat in maze

---
+++ Backtracking/RatInMaze.cpp
@@ -0,0 +1,67 @@
+// Sufiyaan Usmani
+
+#include <iostream>
+#define n 4
+using namespace std;
+
+bool isSafe(int arr[n][n], int x, int y){
+    if((x < n) && (y < n) && arr[x][y] == 1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool ratInMaze(int arr[n][n], int x, int y, int solArr[n][n]){
+    if(x == n -1 && y == n-1){
+        solArr[x][y] = 1;
+        return true;
+    }
+
+    if(isSafe(arr, x, y)){
+        solArr[x][y] = 1;
+
+        if(ratInMaze(arr, x+1, y, solArr)){
+            return true;
+        }
+        if(ratInMaze(arr, x, y+1, solArr)){
+            return true;
+        }
+        solArr[x][y] = 0;
+        return false;
+    }
+    return false;
+}
+
+void print(int arr[n][n]){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main(){
+    int arr[n][n] = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 1, 1}
+    };
+
+    int solArr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            solArr[i][j] = 0;
+        }
+    }
+
+    if(ratInMaze(arr, 0, 0, solArr)){
+        print(solArr);
+    }else{
+        cout << "Cannot solve" << endl;
+    }
+    return 0;
+}
