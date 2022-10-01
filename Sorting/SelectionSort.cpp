fe57001c33b92c2e6b164109c6f60037a3742138
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-10-01 12:02:22 +0500

    added selection sort

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,40 @@
+// Sufiyaan Usmani
+
+#include <iostream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    int i, j, k;
+    for (i = 0; i < size; i++)
+    {
+        k = i;
+        for (j = i; j < size; j++)
+        {
+            if (arr[j] < arr[k])
+            {
+                k = j;
+            }
+        }
+        swap(arr[i], arr[k]);
+    }
+}
+
+void print(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {4, 3, 6, 7, 8, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    sort(arr, size);
+    print(arr, size);
+    return 0;
+}
