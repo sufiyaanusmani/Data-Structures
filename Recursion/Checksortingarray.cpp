1d19c62f183e0d16c178874eab9d38dbc8acea86
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-10-08 16:27:47 +0500

    ignore

--- Recursion/Checksortingarray.cpp
+++ Recursion/Checksortingarray.cpp
@@ -1,24 +1,33 @@
-//Qasim Hasan
-#include<iostream>
+// Qasim Hasan
+#include <iostream>
 using namespace std;
-bool sort(int *arr,int size){
-if(size==0 || size==1){
-    return true;
+bool sort(int *arr, int size)
+{
+    if (size == 0 || size == 1)
+    {
+        return true;
+    }
+    if (arr[0] > arr[1])
+    {
+        return false;
+    }
+    else
+    {
+        bool result = sort(arr + 1, size - 1);
+        return result;
+    }
 }
-if(arr[0]>arr[1]){
-    return false;
-}else{
-    bool result = sort(arr+1,size-1);
-    return result;
-}
-}
-int main(){
-    int size=5;
-    int array[]={2,4,6,8,9};
-    bool ans = sort(array,size);
-    if(ans){
+int main()
+{
+    int size = 5;
+    int array[] = {2, 4, 6, 8, 9};
+    bool ans = sort(array, size);
+    if (ans)
+    {
         cout << " Array Sorted";
-    }else{
+    }
+    else
+    {
         cout << " Array Not Sorted";
     }
     return 0;
