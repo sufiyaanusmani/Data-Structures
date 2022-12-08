08af3b78bfa913b70ad4f9c04678a12ee5f0a3f9
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-12-08 10:15:29 +0500

    minor change

--- Graph/Floyd_Algorithm.cpp
+++ Graph/Floyd_Algorithm.cpp
@@ -30,12 +30,12 @@ void floydWarshall(int dist[][V])
 	vertex no. k is added to the set of
 	intermediate vertices and the set
 	becomes {0, 1, 2, .. k} */
-	for (k = 0; k < V; k++) {
+	for (int k = 0; k < V; k++) {
 		// Pick all vertices as source one by one
-		for (i = 0; i < V; i++) {
+		for (int i = 0; i < V; i++) {
 			// Pick all vertices as destination for the
 			// above picked source
-			for (j = 0; j < V; j++) {
+			for (int j = 0; j < V; j++) {
 				// If vertex k is on the shortest path from
 				// i to j, then update the value of
 				// dist[i][j]
